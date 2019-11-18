ed27a6bafc11d04b3c3f15ee1f0aa6bbd4065d34
Dan Gold <tentativechaos@gmail.com>
2019-11-18 14:08:19 -0800

    Created Household model/schema.

--- models/Household.js
+++ models/Household.js
@@ -2,11 +2,19 @@
 const mongoose = require("mongoose");
 
 // Import Event schema & Member schema
+const Event = require('./Event');
+const Member = require('./Member');
 
 
-// Array of members
+var HouseholdSchema = new mongoose.Schema({
+    // Array of members
+    members: [Member],
+    // Array of events
+    events: [Event]
+});
 
-// Array of events
+// Create model from schema
+const Household = mongoose.model('Household', HouseholdSchema);
 
-
-// Export schema
+// Export model
+module.exports = Household;
