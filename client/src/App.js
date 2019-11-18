3a4fffa68a29f2bc93a49462da6c4f9aa02cd1a4
Jenn Henderson <jenn.henderson18@gmail.com>
2019-11-17 17:35:54 -0800

    remove irrelevant comments

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,7 @@ function App() {
     <Router>
       <div>
           <Switch>
-            {/* Setup route with the exact path that will direct us to the home page imported above */}
             <Route exact path="/" component={Home} />
-            {/* setup a route with the exact path of /saved that will take us to the saved page */}
             <Route exact path="/dashboard" component={Dashboard} />
           </Switch>
         </div>
