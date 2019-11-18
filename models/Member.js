410a33a7ae89c783383d327aa22d9f98f39515f3
Dan Gold <tentativechaos@gmail.com>
2019-11-18 13:15:02 -0800

    Created Member schema in models.

--- models/Member.js
+++ models/Member.js
@@ -1,15 +1,39 @@
 // Import mongoose
 const mongoose = require("mongoose");
 
-// OAuth key? (text)
+var MemberSchema = new MemberSchema({
+    // OAuth key? (text)
+    // TODO: This should be unique, but we also need to allow it to be null when the user is invited, but hasn't created thier account yet
+    // not sure how to do that yet.
+    userOauthKey: {
+        type: String 
 
-// email (text)
+    },
+    // email (text)
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // firtName (text)
+    firstName: {
+        type: String,
+        required: true
+    },
+    // lastName (test)
+    lastName: {
+        type: String
+    },
+    // status ('invited', 'full')
+    status: {
+        type: String,
+        required: true
+    }
 
-// firtName (text)
-
-// lastName (test)
-
-// status ('invited', 'full')
-
+});
 
 // Export schema
+const Member = mongoose.model('Member', MemberSchema);
+
+// Export schema
+module.exports = Member;
