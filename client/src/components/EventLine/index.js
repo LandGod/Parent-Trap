87428ec9614d211e2f13a57d948da164a3ba5b1d
Myles Carey <seanmyles@comcast.net>
2019-11-18 20:34:25 -0800

    added style.css to DashCard and EventLine components

--- client/src/components/EventLine/index.js
+++ client/src/components/EventLine/index.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./style.css";
 
 class EventLine extends Component {
 
   render() {
     return (
       <div className='row'>
-        <p className='col-3'>{this.props.time}
+        <p className='col-2'>{this.props.time}
         </p>
-        <p className='col-3'>{this.props.title}
+        <p className='col-5'>{this.props.title}
         </p>
-        <p className='col-3'>{this.props.duration}
+        <p className='col-2'>{this.props.duration}
         </p>
         <p className='col-3'>{this.props.requestor}
         </p>
