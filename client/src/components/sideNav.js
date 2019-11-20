324b567d883be307f0bdc14777ea5c251d1a3f84
Dan Gold <tentativechaos@gmail.com>
2019-11-19 22:19:14 -0800

    Inital commit to create file for SideNav component.

---
+++ client/src/components/sideNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class SideNav extends Component {
+    render() {
+        return (
+            <div>
+                {/* Do something */}
+            </div>
+        )
+    }
+}
+
+export default SideNav;
