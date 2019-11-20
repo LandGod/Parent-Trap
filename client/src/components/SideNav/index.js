74286bfc1f95c00e6fbe44bb6ad75913d61a6cde
Dan Gold <tentativechaos@gmail.com>
2019-11-20 11:03:55 -0800

    Created basic side-nave element component (without content).

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
-
+import "./style.css";
 
 class SideNav extends Component {
+
+    state = {
+        navWidth='0'
+    };
+
+    openNav = () => {
+        this.setState({ navWidth: '250px' })
+    };
+
+    closeNav = () => {
+        this.setState({ navWidth: '0' })
+    };
+
     render() {
         return (
-            <div>
-                {/* Do something */}
+
+            <div id="mySidenav" class="sidenav" style={navWidth}>
+                
+                <a href="javascript:void(0)" class="closebtn" onclick={closeNav()}>&times;</a>
+                <p>Side Nav example text</p>
+
             </div>
+
         )
     }
 }
