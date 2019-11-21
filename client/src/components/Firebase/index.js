f2cfef82d4b6d94c60c24dd7976c5ca0abc13d58
Jenn Henderson <jenn.henderson18@gmail.com>
2019-11-20 19:52:48 -0800

    light styling

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import "../../pages/style/Home.css"
 
 
 // This is our firebaseui configuration object
 const uiConfig = {
-    signInFlow: 'popup',
+    signInFlow: 'redirect',
     signInSuccessUrl: '/dashboard',
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID
@@ -16,8 +17,6 @@ class SignIn extends Component {
       render() {
         return (
           <div>
-          <h1>My App</h1>
-            <p>Please sign-in:</p>
             <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
           </div>
         )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "../components/Grid/index.js";
-import "./Home.css";
+import "./style/Home.css";
 import SignIn from "../components/Firebase/index"
 
 class Home extends Component {
     render() {
         return (
             <Container>
-                <Row>
-                    <Col size="md-12">
-                        <h1>ParentTrap</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <SignIn />
-                </Row>
-
+                <div className="wrapper">
+                    <Row>
+                        <Col size="md-12">
+                            <h1>ParentTrap</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size="md-12">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facere error quod voluptas repudiandae voluptate! Eos corporis ipsum, laudantium quod quam maxime eligendi nobis beatae illum, asperiores consequatur eaque iusto.</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <SignIn />
+                    </Row>
+                </div>
             </Container>
         )
     }
