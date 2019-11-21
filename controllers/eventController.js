46c4c1c0a08a3de1c8324853b8508bb3799ac06d
Myles Carey <seanmyles@comcast.net>
2019-11-21 15:52:30 -0800

    included my old routes/controller code for my local sand-box until they are replaced

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -37,3 +37,45 @@ module.exports = {
     });
   }
 };
+
+// old controllers for sand-box
+const mongoose = require("mongoose");
+
+// Defining methods for the eventController
+module.exports = {
+  findAll: function(req, res) {
+    db.Event.find(req.query)
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  },
+  findAllPopulated: function(req, res) {
+    db.Event.find(req.query)
+      .sort('+startTime')
+      .populate("creator")
+      .populate("invitees.member")
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Event.findById(req.params.id)
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Event.create(req.body)
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    var id = mongoose.Types.ObjectId(req.params.id);
+    db.Event.findOneAndUpdate({ _id: id }, req.body, {new: true})
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Event.findById(req.params.id)
+      .then(dbEvent => dbEvent.remove())
+      .then(dbEvent => res.json(dbEvent))
+      .catch(err => res.status(422).json(err));
+  }
+};
--- routes/api/events.js
+++ routes/api/events.js
@@ -42,4 +42,27 @@ router
       });
   });
 
+
+  // old routes for sand-box
+  const {findAll, findAllPopulated, create, findById, update, remove} = require("../../controllers/eventController");
+
+  // Matches with "/api/events"
+  router.route("/")
+  .get(findAll)
+  .post(create);
+
+  // Matches with "/api/events/members"
+  router.route("/members")
+  .get(findAllPopulated);
+
+
+  // Matches with "/api/events/:id"
+  router
+  .route("/:id")
+  .get(findById)
+  .put(update)
+  .delete(remove);
+
+
+
 module.exports = router;
