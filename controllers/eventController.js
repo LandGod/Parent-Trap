39f9206f572a1d7b702204901802e7896d3334e0
Dan Gold <tentativechaos@gmail.com>
2019-11-20 21:21:10 -0800

    Fixed missing firebase dependencies.

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,16 @@
-// placeholder file for the time being
+const db = require('../models');
 
-// const db = requireI('../models');
+module.exports = {
 
-// Define CRUD methods for eventsController
+    // Return all events for the given household and populate all member references
+    findAllEventsPopulated: function(householdId) {
+        db.Household.find({_id: householdId})
+          .sort('+startTime')
+          .populate("events")
+          .populate("creator")
+          .populate("invitees.member")
+          .then(dbEvent => res.json(dbEvent))
+          .catch(err => res.status(422).json(err));
+      },
+
+};
