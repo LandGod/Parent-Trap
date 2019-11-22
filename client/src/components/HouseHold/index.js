df39987f89d36cbcd5e5443d180f007b70de22ad
Dan Gold <tentativechaos@gmail.com>
2019-11-22 15:10:50 -0800

    Added MemberFormRow subcomponent in HouseHold component folder. Added style.css with round button styles.

--- client/src/components/HouseHold/index.js
+++ client/src/components/HouseHold/index.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import "./style.css";
 
+// Household create/edit component
 class HouseHold extends Component {
-  state = {
-    
-  };
+  state = {};
+
 
 
   render() {
     return (
       <div>
-          
+        <form>
+            <div className="row">
+                <h2>{this.props.createOrEdit} Household</h2>
+            </div>
+          <div className="form-group row">
+            <label for="householdNameInput" className="col-sm-2 col-form-label">
+              Household Name:
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                id="householdNameInput"
+                placeholder="Smith"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <h3>Add/Edit Members </h3>
+          </div>
+          {/* Dynamic add/edit lines go here */}
+        </form>
       </div>
     );
   }
 }
+
+// Set defaults for props:
+HouseHold.defaultProps = { 
+    createOrEdit = 'Create'
+ }
 
 export default SideNav;
