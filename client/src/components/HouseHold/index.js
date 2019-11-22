564fe8ebae6c5cc0c8f28625b6f4d61eba300aea
Dan Gold <tentativechaos@gmail.com>
2019-11-22 13:07:41 -0800

    Created initial component file for HouseHold.

---
+++ client/src/components/HouseHold/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class HouseHold extends Component {
+  state = {
+    
+  };
+
+
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default SideNav;
