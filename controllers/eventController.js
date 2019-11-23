4b3d1fe43159bb6e7b3d999a2a365b799c6c4de4
Dan Gold <tentativechaos@gmail.com>
2019-11-23 12:40:29 -0800

    Removed sandbox-only methods

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -39,43 +39,6 @@ module.exports = {
       .then((result) => {resolve(result)})
       .catch((err) => {reject(err)});
     });
-  },
-
-  // for sandbox only:
-  findAll: function(req, res) {
-    db.Event.find(req.query)
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
-  },
-  findAllPopulated: function(req, res) {
-    db.Event.find(req.query)
-      .sort('+startTime')
-      .populate("creator")
-      .populate("invitees.member")
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Event.findById(req.params.id)
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Event.create(req.body)
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    var id = mongoose.Types.ObjectId(req.params.id);
-    db.Event.findOneAndUpdate({ _id: id }, req.body, {new: true})
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Event.findById(req.params.id)
-      .then(dbEvent => dbEvent.remove())
-      .then(dbEvent => res.json(dbEvent))
-      .catch(err => res.status(422).json(err));
   }
 
 };
