f57c261e96b01b105ed43bf76137a0d6ece00ae6
Dan Gold <tentativechaos@gmail.com>
2019-11-23 01:30:50 -0800

    Set up dynamic generation of MemberFormRow components. Set up add/remove functions for rows. Set up check userId dummy function. Added some dummy data for testing.

--- client/src/components/HouseHold/index.js
+++ client/src/components/HouseHold/index.js
@@ -5,20 +5,84 @@ import "./style.css";
 // Household create/edit component
 class HouseHold extends Component {
   state = {
+    mode: this.props.mode || "create",
+    members: [
+      // Dummy data until API is set up
+      {
+        _id: "5dd6ea9f6c0c7213542d089a",
+        firstName: "James",
+        lastName: "Holden",
+        email: "captain@rocinante.com",
+        status: "full"
+      },
+      {
+        _id: "5dd6ea9f6c0c7213542d089b",
+        firstName: "Naomie",
+        lastName: "Nagata",
+        email: "xo@rocinante.com",
+        status: "full"
+      },
+      {
+        _id: "5dd6ea9f6c0c7213542d089c",
+        firstName: "Clarisa",
+        lastName: "Mao",
+        email: "peaches@rocinante.com",
+        status: "full"
+      }
+    ]
+  };
 
+  isCurrentUser = id => {
+    // Compare supplied id to id of currently logged in user
+    // For debug purposes this function will return true for a set value
+    if (id === "5dd6ea9f6c0c7213542d089a") {
+      return true;
+    }
+    return false;
+  };
+
+  // Adds a blank user object to the state
+  addMemberRow = event => {
+    event.preventDefault();
+    this.setState(currentState => {
+      currentState.members.push({
+        firstName: "",
+        lastName: "",
+        email: "",
+        status: "invited"
+      });
+
+      return currentState;
+    });
+  };
+
+  // Removes the user object from state which coresponds to the row on which the button was clicked
+  removeRow = (event, i) => {
+    event.preventDefault();
+    this.setState(currentState => {
+      currentState.members.splice(i, 1);
+      return currentState;
+    });
   };
 
   render() {
     return (
       <div>
         <form>
-            <div className="row justify-content-center">
-                <div className="col-md-4">
-                <h2 className="text-center">{this.props.createOrEdit} Household</h2>
-                </div>
+            {/* Title */}
+          <div className="row justify-content-center">
+            <div className="col-md-4">
+              <h2 className="text-center">
+                {this.props.createOrEdit} Household
+              </h2>
             </div>
+          </div>
+          {/* Household Name Input */}
           <div className="form-group row justify-content-center">
-            <label htmlFor="householdNameInput" className="col-sm-2 col-form-label">
+            <label
+              htmlFor="householdNameInput"
+              className="col-sm-2 col-form-label"
+            >
               <h5>Household Name:</h5>
             </label>
             <div className="col-sm-4">
@@ -30,26 +94,31 @@ class HouseHold extends Component {
               />
             </div>
           </div>
+          {/* Member input */}
           <div className="row">
             <h3>Add/Edit Members </h3>
           </div>
-          {/* Add top row with current user info that is already locked in if this is the create page */}
-          <MemberFormRow 
-            firstName={'James'}
-            lastName={'Holden'}
-            email={'captain@rocinante.org'}
-            readOnly={true}
-            showAddButton={false}
-          />
-          {/* Dynamic add/edit lines go here */}
-          <MemberFormRow 
-            firstName={'Fred'}
-            lastName={'Johnson'}
-            email={'F.Johnson@tycho.com'}
-            showAddButton={true}
-            addNext={(event) => {event.preventDefault(); console.log('Add new row button activated!')}}
-            removeSelf={(event) => {event.preventDefault(); console.log('Remove self button activated!')}}
-          />
+          {this.state.members.map((member, i) => {
+            return (
+              <MemberFormRow
+                key={i}
+                firstName={member.firstName}
+                lastName={member.lastName}
+                email={member.email}
+                // Add readonly attribute if member object matches currently logged in member (You can't delete yourself)
+                readOnly={this.isCurrentUser(member._id)}
+                // If this is the last member in the list, show the add button
+                // showAddButton={
+                //   i + 1 >= this.state.members.length ? true : false
+                // }
+                showAddButton={!this.isCurrentUser(member._id)}
+                addNext={this.addMemberRow}
+                removeSelf={event => {
+                  this.removeRow(event, i);
+                }}
+              />
+            );
+          })}
         </form>
       </div>
     );
@@ -57,8 +126,8 @@ class HouseHold extends Component {
 }
 
 // Set defaults for props:
-HouseHold.defaultProps = { 
-    createOrEdit: 'Create'
- }
+HouseHold.defaultProps = {
+  createOrEdit: "Create"
+};
 
 export default HouseHold;
