a154ea483722b40c8c8a45808f29f98925c7bbcd
Jenn Henderson <jenn.henderson18@gmail.com>
2019-11-23 11:17:38 -0800

    storing information in session storagE

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -10,7 +10,34 @@ const uiConfig = {
     signInSuccessUrl: '/dashboard',
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
+  ],   
+  callbacks: {
+    signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+      
+      // store the user Auth key in session storage
+      sessionStorage.setItem('userAuth',authResult.user.uid);
+      
+      // store the user display name
+      var displayName = authResult.user.displayName
+      
+      // split the display name to first and last name
+      var displayNameArray = displayName.split(" ");
+      var firstName = displayNameArray[0];
+      var lastName = displayNameArray[1];
+
+      // store the information in session storage
+      sessionStorage.setItem('firstName', firstName);
+      sessionStorage.setItem('lastName', lastName);
+
+      // if user exists find the household ID and send to dashboard
+      // if user does not exist send to the create household screen
+      
+      
+      
+      // this.props.history.push('/dashboard');
+      // return false
+    }
+  }
 };
 
 class SignIn extends Component {
