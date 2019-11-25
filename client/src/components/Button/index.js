fc33f62223d52e2c69609257d3d72d7998b475cc
mylescarey2019 <50058682+mylescarey2019@users.noreply.github.com>
2019-11-25 07:55:54 -0800

    Update index.js
    removed modal import and removed <div> wrapper

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import "./style.css";
-import ModalCardBody from '../Modal/modalBody';
-
 
 class Button extends Component {
 
   render() {
     return (
-      <div>
       <button id={this.props.id} onClick={(event) => this.props.clickEvent(event,this.props.identifier)} className="">
         <i className={`${this.props.icon}`} 
            aria-hidden="true" /> {this.props.title}
       </button>
-      </div>
     )
   }
 }
