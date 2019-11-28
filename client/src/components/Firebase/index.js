293edd2e3b1d7bfdf00ca4dcab86f7540de4608a
Dan Gold <tentativechaos@gmail.com>
2019-11-27 17:00:08 -0800

    Added redirect logic to Firebase/index.js and fixed some package.json issues.

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -1,54 +1,50 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import React, { Component } from "react";
+import * as firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import API from "../../utils/API";
-import "../../pages/style/Home.css"
-
+import "../../pages/style/Home.css";
 
 // This is our firebaseui configuration object
 const uiConfig = {
-    signInFlow: 'redirect',
-    signInSuccessUrl: '/dashboard',
-    signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],   
+  signInFlow: "redirect",
+  signInSuccessUrl: "/dashboard",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
     signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-      
       // store the user Auth key in session storage
-      sessionStorage.setItem('id',authResult.user.uid);
-      
+      sessionStorage.setItem("id", authResult.user.uid);
+
       // store the user display name
-      var displayName = authResult.user.displayName
+      var displayName = authResult.user.displayName;
 
       // store the user email
-      var email = authResult.user.email
+      var email = authResult.user.email;
 
       // store email in session storage
-      sessionStorage.setItem('email', email);
-      
+      sessionStorage.setItem("email", email);
+
       // split the display name to first and last name
       var displayNameArray = displayName.split(" ");
       var firstName = displayNameArray[0];
       var lastName = displayNameArray[1];
 
       // store the information in session storage
-      sessionStorage.setItem('firstName', firstName);
-      sessionStorage.setItem('lastName', lastName);
+      sessionStorage.setItem("firstName", firstName);
+      sessionStorage.setItem("lastName", lastName);
 
       // retrive the information from session storage and store in an object to send through the API call
       var userData = {
-        id: sessionStorage.getItem('id'),
-        firstName: sessionStorage.getItem('firstName'),
-        lastName: sessionStorage.getItem('lastName'),
-        email: sessionStorage.getItem('email')
-      }
+        id: sessionStorage.getItem("id"),
+        firstName: sessionStorage.getItem("firstName"),
+        lastName: sessionStorage.getItem("lastName"),
+        email: sessionStorage.getItem("email")
+      };
 
       // Send API call to evaluate:
       API.login(userData)
-        .then(res => console.log('success', res))
+        .then(res => console.log("success", res))
         .catch(err => console.log(err));
-      
+
       // this.props.history.push('/dashboard');
       // return false
     }
@@ -56,14 +52,29 @@ const uiConfig = {
 };
 
 class SignIn extends Component {
-      render() {
-        return (
-          <div>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
-          </div>
-        )
-      }
+  state = {
+    redirectDashboard: false, // Set this to true to redirect to dashboard
+    redirectSignUp: false // Set this to true to redirect to signup flow
+  };
+
+  render() {
+    if (this.state.redirectDashboard) {
+      return <Redirect to="/dashboard" />;
+    }
+
+    if (this.state.redirectDashboard) {
+      return <Redirect to="/signup" />;
+    }
+
+    return (
+      <div>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      </div>
+    );
+  }
 }
 
-export default SignIn
-    
+export default SignIn;
