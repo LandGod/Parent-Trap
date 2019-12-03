e391547d4c6bd0a81e0582d96622aaa468fb9c32
Dan Gold <tentativechaos@gmail.com>
2019-12-02 18:15:30 -0800

    Updated getLocalUserInfo to grab all the info now stored in session storage.

--- client/src/components/utilityFunctions/index.js
+++ client/src/components/utilityFunctions/index.js
@@ -1,14 +1,13 @@
 module.exports = {
   // Get user info object from client storage
-  // The oauthKey can be accessed from the returned object using [oauthKey] or [id] to allow for
-  // personal preference and avoid confusion (since it is called different things in different places)
   getLocalUserInfo: function() {
     return {
       firstName: sessionStorage.getItem("firstName"),
       lastName: sessionStorage.getItem("lastName"),
       email: sessionStorage.getItem('email'),
-      oauthKey: sessionStorage.getItem("id"),
-      id: sessionStorage.getItem("id")
+      oauthKey: sessionStorage.getItem("id"), // Oauth id
+      memberId: sessionStorage.getItem("userID"), // Mongoose object id for memeber document
+      currentHouseholdId: sessionStorage.getItem("householdId") // Mongoose object id for current household
     };
   }
 };
