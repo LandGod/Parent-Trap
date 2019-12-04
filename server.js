221b13ba7852dd8dc9fd66ab537f96d641575147
Dan Gold <tentativechaos@gmail.com>
2019-12-04 14:40:48 -0800

    Lots of changes to allow for deployment with aws.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.HTTP_PORT || 3001;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
