c32d925ff766764974e8d15fa74c0d57301a8b50
Dan Gold <tentativechaos@gmail.com>
2019-12-04 22:24:48 -0800

    Hail mary 3: Tokyo Drift

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  IndexRoute
-} from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import SignUp from "./pages/Signup";
-import Home from "./pages/Home";
-import withFirebaseAuth from "react-with-firebase-auth";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "./components/Firebase/firebaseConfig";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import SignUp from './pages/Signup';
+import Home from './pages/Home';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './components/Firebase/firebaseConfig';
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   render() {
+
     return (
-      <Switch>
-        <Router history={hasHistory}>
-          <Route path="/" component={Home}>
-            <IndexRoute component={Dashboard} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/signup" component={SignUp} />
-          </Route>
-        </Router>
-      </Switch>
+      <Router>
+        <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route path="/dashboard?*" component={Dashboard} />
+              <Route exact path="/signup" component={SignUp}  />
+            </Switch>
+          </div>
+      </Router>
     );
   }
 }
 
+
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider()
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
 export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth
+    providers,
+    firebaseAppAuth,
 })(App);
