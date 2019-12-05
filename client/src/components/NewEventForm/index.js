f89bb60620fd17b0d30fc93070bf7a9da7b2d5f7
Dan Gold <tentativechaos@gmail.com>
2019-12-05 15:34:43 -0800

    Fixed incorrect preventDefault call (missing parenthesis).

--- client/src/components/NewEventForm/index.js
+++ client/src/components/NewEventForm/index.js
@@ -321,9 +321,9 @@ export class NewEventTable extends Component {
                     <Form.Control as="textarea" rows="3" placeholder='We are picking up Amy on the way to practice!' onChange={(e) => this.handleDetailsInput(e)} value={this.state.eventDetails} />
                 </Form.Group>
 
-                <button className='btn btn-default' onClick={(e) => {e.preventDefault; this.handleSubmit(e)}}>Submit</button>
+                <button className='btn btn-default' onClick={(e) => {e.preventDefault(); this.handleSubmit(e)}}>Submit</button>
 
-                <button className='btn btn-default' data-dismiss="modal" onClick={(e) => {e.preventDefault; this.props.modalClose(e)}} >Cancel</button>
+                <button className='btn btn-default' data-dismiss="modal" onClick={(e) => {e.preventDefault(); this.props.modalClose(e)}} >Cancel</button>
 
                 {/* <Alert color='alert' isOpen={this.state.inputErrorAlert} toggle={this.toggleErrorModal.bind(this)}> Please Check the Input</Alert> */}
             </Form>
