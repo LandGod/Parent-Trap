f9c9fef90dc91f77549c3c0f5c86eeb006eb140d
Dan Gold <tentativechaos@gmail.com>
2019-12-04 21:20:29 -0800

    Build project

---
+++ client/build/precache-manifest.0aace6bc25d4a5b54d3953fcde997e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00c20079ee2d55669bfcdf6e6fc0ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "579292f173908a502b87",
+    "url": "/static/css/main.f56b6ce2.chunk.css"
+  },
+  {
+    "revision": "3bf148c9cfb0cdf58eb9",
+    "url": "/static/js/2.435ca183.chunk.js"
+  },
+  {
+    "revision": "579292f173908a502b87",
+    "url": "/static/js/main.921eeaa3.chunk.js"
+  },
+  {
+    "revision": "12a5e39a249c42ad49aa",
+    "url": "/static/js/runtime-main.69c3d5bf.js"
+  }
+]);
