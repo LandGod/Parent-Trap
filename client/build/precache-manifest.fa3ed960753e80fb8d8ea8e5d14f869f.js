0516266c8bb2fd3b4fc9ad747f013894ab8d2b32
Dan Gold <tentativechaos@gmail.com>
2019-12-04 17:07:17 -0800

    More attempts at making this work with aws elastic beanstalk.

---
+++ client/build/precache-manifest.fa3ed960753e80fb8d8ea8e5d14f869f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b62d5dfa7070a9904a91a46d23cae3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0faaf296e37183b9ac71",
+    "url": "/static/css/main.e3a11092.chunk.css"
+  },
+  {
+    "revision": "b773fd7fd259d4b6b062",
+    "url": "/static/js/2.bf7ef67e.chunk.js"
+  },
+  {
+    "revision": "0faaf296e37183b9ac71",
+    "url": "/static/js/main.ba0e5b3e.chunk.js"
+  },
+  {
+    "revision": "12a5e39a249c42ad49aa",
+    "url": "/static/js/runtime-main.69c3d5bf.js"
+  }
+]);
