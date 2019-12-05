f1c0362efab9e8bb21b2903409b7b4e804b5c7c0
Dan Gold <tentativechaos@gmail.com>
2019-12-04 22:11:17 -0800

    Another Hail Marry! Woooooogit push heroku master

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import SignUp from './pages/Signup';
-import Home from './pages/Home';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './components/Firebase/firebaseConfig';
-
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  IndexRoute
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/Signup";
+import Home from "./pages/Home";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./components/Firebase/firebaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   render() {
-
     return (
-      <Router>
-        <div>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/dashboard" component={Dashboard} />
-              <Route path="/signup" component={SignUp}  />
-            </Switch>
-          </div>
+      <Router history={hasHistory}>
+        <Route path="/" component={Home}>
+          <IndexRoute component={Dashboard} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/signup" component={SignUp} />
+        </Route>
       </Router>
     );
   }
 }
 
-
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider()
 };
 
 export default withFirebaseAuth({
-    providers,
-    firebaseAppAuth,
+  providers,
+  firebaseAppAuth
 })(App);
